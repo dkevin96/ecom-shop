c11d73bce40b3ebc45949365de721a420bb59d3b
artemis <57104807+dkevin96@users.noreply.github.com>
2021-06-05 16:47:39 +0200

    Add register sucess Page, modify database sql

--- server/client/src/components/login/RegisterSuccess.js
+++ server/client/src/components/login/RegisterSuccess.js
@@ -1,10 +1,31 @@
 import { useRef, useState, useEffect } from "react";
 import apiAxios from "../../config/axiosConfig";
+import { useHistory } from "react-router-dom";
+import { Typography, Button } from "antd";
+
+const { Title, Text } = Typography;
 
 const RegisterSuccess = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/");
+  };
   return (
-    <div>
-      <h1>Account Created</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
+      <Title level={3}>Thank you</Title>
+      <Text type="success">Account created successfully</Text>
+      <br/>
+      <Button type="link" onClick={handleClick}>
+        Click here to return to Dashboard
+      </Button>
     </div>
   );
 };
