61ff7d5fccff67971d747a05c4c7c2f8d3d600f5
artemis <57104807+dkevin96@users.noreply.github.com>
2021-06-17 18:47:10 +0200

    deploy4

--- server/config/index.js
+++ server/config/index.js
@@ -1,19 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 // require('dotenv').config({path: __dirname + '/.env'})
-const {Pool} = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const { Pool } = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 // postgresql://api_user:password@localhost:5432/ecom
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  // ssl: {
-  //   rejectUnauthorized: false
-  //   }
-})
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
-    port: parseInt(process.env.PORT, 10),
-    pool
-}
+  port: parseInt(process.env.PORT, 10),
+  pool,
+};
