eeb3afbed4aa712c963ddd6e7dd41b2d9353453a
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-13 23:29:16 +0100

    Add frontend vercel proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+  const target = process.env.REACT_APP_API_URL || 
+    (process.env.NODE_ENV === 'production' 
+      ? 'https://react-express-ecom-shop-api.vercel.app/'
+      : 'http://host.docker.internal:5000');
+
   const proxy = createProxyMiddleware('/api', {
-    target: 'http://host.docker.internal:5000',
+    target,
     changeOrigin: true,
   });
   app.use(proxy);
